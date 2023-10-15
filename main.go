afc0061b3ed037c68d37aa31d3d3174268e37bab
SkyrimForest <835766751@qq.com>
2023-10-15 17:46:58 +0800

    second commit,Context and Router added.

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"gim"
 	"net/http"
 )
 
 func main() {
 	r := gim.New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	r.GET("/", func(c *gim.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.GET("/hello", func(c *gim.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
-
+	r.POST("/login", func(c *gim.Context) {
+		c.JSON(http.StatusOK, gim.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
 	r.Run(":9999")
 }
