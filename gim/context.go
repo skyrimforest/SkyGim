ce187435cc870b80ab66fa9fa07a6e2e8373bbc3
SkyrimForest <835766751@qq.com>
2023-10-16 21:11:58 +0800

    Add trie to support dynamic router

--- gim/context.go
+++ gim/context.go
@@ -9,12 +9,16 @@ import (
 type H map[string]interface{}
 
 type Context struct {
+	//origin obj
 	Writer http.ResponseWriter
 	Req    *http.Request
 
+	//request info
 	Path   string
 	Method string
+	Params map[string]string
 
+	//response info
 	StatusCode int
 }
 
@@ -25,6 +29,11 @@ func newContext(w http.ResponseWriter, req *http.Request) *Context {
 		Path:   req.URL.Path,
 		Method: req.Method,
 	}
+}
+
+func (c *Context) Param(key string) string {
+	value, _ := c.Params[key]
+	return value
 }
 
 func (c *Context) PostForm(key string) string {
--- main.go
+++ main.go
@@ -13,6 +13,14 @@ func main() {
 	r.GET("/hello", func(c *gim.Context) {
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
+	r.GET("/hello/:name", func(c *gim.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *gim.Context) {
+		c.JSON(http.StatusOK, gim.H{"filepath": c.Param("filepath")})
+	})
 	r.POST("/login", func(c *gim.Context) {
 		c.JSON(http.StatusOK, gim.H{
 			"username": c.PostForm("username"),
